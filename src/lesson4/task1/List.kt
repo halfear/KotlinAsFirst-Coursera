e109a89a36fbf57c6ece54a48e92a1fb0512bd18
Pavel Polukhin <halfear@mail.ru>
2019-02-20 18:24:49 +0700

    ageDescription solved timeForHalfWay solved whichRookThreatens solved rookOrBishopThreatens solved triangleKind solved All ifElse solved Fixed ageDescription and timeForHalfWay isNumberHappy solved All Logical solved ageDescription fixed lesson3 to isCoPrime solved squareBetweenExists solved collatzSteps solved sin solved sin changed squareSequenceDigit solved without fib Long (Let it be Int) digitNumber fixed sin probably //commented revert solved
    Lesson4
    mean solved

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -122,7 +122,12 @@ fun abs(v: List<Double>): Double = TODO()
  *
  * Рассчитать среднее арифметическое элементов списка list. Вернуть 0.0, если список пуст
  */
-fun mean(list: List<Double>): Double = TODO()
+fun mean(list: List<Double>): Double {
+    return when {
+        list.size == 0 -> 0.0
+        else -> list.sum() / list.size
+    }
+}
 
 /**
  * Средняя
